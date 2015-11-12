466b27ed4e67169e5053411d011b8652e119a3da
github username <github email address>
2015-11-11 19:10:22 -0500

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,14 +1,12 @@
 require 'pry'
 
 def my_all?(collection)
-  # return collection if collection.empty?
-
   i = 0
   collect = []
   while i < collection.size
     collect << yield(collection[i])
     i += 1
   end
-  
+
   collect.include?(false) ? false : true
 end
