56f781e4566d9e6275ceeac4875b7aec4b1cd886
Lukeghenco <lukeghenco@gmail.com>
2015-12-13 12:55:25 -0500

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
+#my_all to see if each item in the array is less than 2
 def my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i += 1
+  end
 
+  #inlcude? to determine the return value of the #my_all?
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
+
+
