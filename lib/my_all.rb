79a2e693ebdfc139748d00f62a28139b1bf568e0
febbraiod <donedesign1@gmail.com>
2015-12-14 22:54:00 -0500

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,4 +2,13 @@ require 'pry'
 
 def my_all?(collection)
 
+  idx = 0
+  while idx < collection.length
+    if !yield(collection[idx])
+      return false
+    end
+    idx += 1
+  end
+
+  true
 end
