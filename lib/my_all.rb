888f6588606caa396b2db9eb9a1a19e4c628c74a
zacscodingclub <zbaston@gmail.com>
2015-12-26 14:23:04 -0600

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,10 @@
-require 'pry'
-
 def my_all?(collection)
+  i = 0
+  saved_values = []
+  while i < collection.length
+    saved_values << yield(collection[i])
+    i += 1
+  end
 
+  saved_values.include?(false) ? false : true
 end
