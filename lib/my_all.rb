3c509ab54c5446ce4f278d084babc2e6a76ec892
azheng249 <andy.zheng249@gmail.com>
2016-01-04 01:11:26 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_all?(collection)
-
+  my_all = true
+  i = 0
+  while i < collection.length
+    my_all = yield collection[i]
+    break if my_all == false
+    i += 1
+  end
+  my_all == true ? true : false
 end
