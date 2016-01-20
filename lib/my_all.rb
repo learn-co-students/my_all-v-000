e6f7f1412d2311ea9aa43907d2422a58511c89a5
smithWEBtek <brad@smithwebtek.com>
2016-01-19 22:41:30 -0500

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,18 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  asdf = []
+  while i < collection.length
+    asdf << yield(collection[i])
+    i+=1
+  end 
 
+  if asdf.include?(false)
+    false
+  else
+    true
+  end
 end
+
+# my_all?([1,2,3]) {|item| item < 2 }
