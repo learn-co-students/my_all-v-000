f5319404b13e35e08de6b454c7146ce6f6137abd
bradleyoashburn <bradleyoashburn@gmail.com>
2016-03-04 00:24:01 -1000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  results = []
 
+  while i < collection.length
+    results.push(yield collection[i])
+    i += 1
+  end
+
+  results.include?(false) ? false : true
 end
