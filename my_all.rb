7232d82ed09274668e1af6d826425ed4d030e384
Robert Koeze <rkoeze@gmail.com>
2016-03-10 14:29:44 -0800

    Done.

--- my_all.rb
+++ my_all.rb
@@ -1,16 +1,16 @@
-require 'pru'
+require 'pry'
 
 def my_all?(collection)
   i = 0
-  block_return_values = []
+  block_return_values = Array.new
   while i < collection.length
     block_return_values << yield(collection[i])
-    i = i + 1
+    i += 1
   end
 
   if block_return_values.include?(false)
-    false
+    return false
   else
-    true
+    return true
   end
 end
