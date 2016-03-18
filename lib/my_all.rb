5ad2c1ffdabf53f477cd2c5db4b8db184d3b20d9
miclp <michaelpeimani@gmail.com>
2016-03-17 21:51:36 -0700

    wrote #my-all

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,34 @@
 require 'pry'
 
 def my_all?(collection)
+  if block_given?
+    i = 0
+    block_return_values = []
+    while i < collection.length
+      block_return_values << yield(collection[i])
+      i += 1
+    end
 
+    # Using #include?
+#    if block_return_values.include?(false)
+#      false
+#    else
+#      true
+#    end
+
+    # Alternate approach without using #include?
+    i = 0
+    while i < block_return_values.length
+      if block_return_values[i] == false
+        return false
+      else
+        i += 1
+      end
+    end
+    return true
+
+  else
+    puts "no block given"
+    return nil
+  end
 end
