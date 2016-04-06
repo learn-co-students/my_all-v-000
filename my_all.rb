d66b7336395b67a34624cc9c23726dc02830db9a
ludaires <ludaires@gmail.com>
2016-04-06 15:44:30 -0400

    Done.

--- my_all.rb
+++ my_all.rb
@@ -1,11 +1,11 @@
-def my_all(collection)
+def my_all?(collection)
   i=0
   all = []
   while i < collection.length
     all << yield(collection[i])
     i+=1
   end
-
+  binding.pry
   if all.include? false
     false
   else
