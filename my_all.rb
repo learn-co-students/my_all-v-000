63c63093ec327d8d3b7279de90a048005a225f39
dmm89 <dmendez89@gmail.com>
2016-05-10 12:21:58 -0400

    Done.

--- my_all.rb
+++ my_all.rb
@@ -1,12 +1,14 @@
-def my_all(collection)
-  i=0
-  all = []
-  while i < collection.length
-    all << yield(collection[i])
-    i+=1
-  end
+require 'pry'
 
-  if all.include? false
+def my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i += 1
+  end
+ 
+  if block_return_values.include?(false)
     false
   else
     true
