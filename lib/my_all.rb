6e4959206d8febc1513bcb4ac753d734be1557c4
Chenxi Wu <wuchenxi2013@gmail.com>
2016-10-27 10:22:01 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
-def my_all?(collection)
-
+def my_all?(array)
+  i = 0
+  while i < array.length
+    if !yield(array[i])
+      break
+    end
+    i+=1
+  end
+  i==array.length
 end
