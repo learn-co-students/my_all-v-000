36321050ff7779473513858d8b4be8beea59370f
Damien de Lartigue <damien@delartigue.co.uk>
2017-01-26 20:21:16 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  arr = []
+  while i < collection.length
+    arr << yield(collection[i])
+    i += 1
+  end
 
+  arr.include?(false) ? false : true
 end
