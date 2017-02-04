d7af3f6062df5365f4de584d3e82fdf439b0b4b3
Hui Jiang <hui1021@yahoo.com>
2017-02-04 19:21:33 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  new_array = []
+  while i < collection.length
+      new_array<< yield(collection[i])
+      i = i + 1
+  end
+
+  if new_array.include?(false)
+      false
+  else
+      true
+  end
 
 end
