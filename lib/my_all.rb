04d49f1fa70789da0175527d511f919509830e97
Jelo Yambao <jelolearns@gmail.com>
2017-05-23 05:23:01 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  is_even = true
+  while i < collection.length
+    if !yield(collection[i])
+      is_even = false
+    end
+    i += 1
+  end
+  is_even
+end
 
+my_all?([2,1,3]) do |i|
+  i.even?
 end
