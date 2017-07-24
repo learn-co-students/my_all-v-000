32414531181875a18e5428ceea1717ecdcb294eb
Mariel F <marielbeth@gmail.com>
2017-07-24 13:48:34 +0000

    finished codealong and added another #my_all? method

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,29 @@
 require 'pry'
 
-def my_all?(collection)
+# def my_all?(collection)
+#   i = 0
+#   block_return_values = []
+#   while i < collection.length
+#     block_return_values << yield(collection[i])
+#     i += 1
+#   end
+#
+#   if block_return_values.include?(false)
+#     false
+#   else
+#     true
+#   end
+# end
 
+# created a simpler solution that doesn't use #include? method
+def my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    if yield(collection[i]) == false
+      return false
+    end
+    i += 1
+  end
+  true
 end
