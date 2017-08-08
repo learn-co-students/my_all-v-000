1a0ed5ec004f41c2768c39c31c801f0a5d3e11b2
Jennifer Nelson <jennifernelson118@gmail.com>
2017-08-08 21:57:30 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,18 @@
 require 'pry'
 
-def my_all?(collection)
+# Creating an #all? method without using #all? #
 
+def my_all?(collection)
+  i = 0
+  return_values = []
+  while i < collection.length
+    return_values << yield(collection[i])
+    i += 1
+  end #code will return an array of true and false if we stopped here.
+  if return_values.include?(false) #since #all? returns false if any iteration is false, we need to add
+     #include? to get a true or false return.
+    false
+  else
+    true
+  end
 end
