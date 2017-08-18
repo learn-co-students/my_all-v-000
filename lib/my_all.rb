410e2f576783c0d4024d7a605ebb5ec5f33cfadb
Sofia Candiani <sofia.candiani123@gmail.com>
2017-08-18 22:04:13 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
-require 'pry'
-
 def my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    #value added to block_return_values array
+    #its added to this array because it can store multiple answers
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+  
+  if block_return_values.include?(false)
+   false
+ else
+   true
+ end
 
 end
