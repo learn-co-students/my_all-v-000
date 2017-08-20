e0c6c510dab138578a692caf8c30099c961074a4
Lara Sanders <larasanders@gmail.com>
2017-08-20 17:51:10 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,12 +7,10 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i = i + 1
   end
-end
+
 if block_return_values.include?(false)
   false
 else
   true
 end
 end
-
-my_all?([1, 2, 3]) {|i| i < 2}
