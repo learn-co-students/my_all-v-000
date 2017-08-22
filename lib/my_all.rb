7bd3fe6d0cb027e5bc32bf7026e9e423a09fb405
Sally Zasloff <szasloff@gmail.com>
2017-08-22 17:54:13 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,10 +4,9 @@ def my_all?(collection)
   i = 0
   block_return_values=[]
   while i < collection.length
-    block_return_values<<yield(collection[i])
+    block_return_values << yield(collection[i])
     i += 1
   end
-end
 
   if block_return_values.include?(false)
     false
