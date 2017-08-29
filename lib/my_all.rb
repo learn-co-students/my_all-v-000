b53c00d9965233279bb3af4f205c1e6bd903f1db
github username <github email address>
2017-08-28 17:27:59 -0700

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,29 @@
-require 'pry'
+
 
 def my_all?(collection)
+    i = 0
+    block_return_values = []
+    while i < collection.length
+      i = i + 1
+      block_return_values <<  yield(collection[1])
+    end
+    if block_return_values.include?(false)
+      false
+    else
+      true
+  end
+end
+def my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
