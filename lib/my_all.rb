a1686688e53f1d9039e020d841a66572961ac46a
Lisa Lee <lisa_lee@brown.edu>
2017-09-25 05:23:25 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  array = []
+  while i < collection.length
+    array << yield(collection[i])
+    i += 1
+  end
+  if array.include?(false)
+    false
+  else
+    true
+  end
 end
