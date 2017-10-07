6e429b7c292bd223af4dcbbecddcc64abf591ae3
Aaron Lieberman <aaronmaxmusic@gmail.com>
2017-10-07 18:29:21 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,10 +4,9 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length
-    block_return_values <<  yield(collection[i])
-    i += 1   #i += 1 does the same thing. Use this if it's easier for you.
+    block_return_values << yield(collection[i])
+    i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
   end
-end
 
 if block_return_values.include?(false)
    false
