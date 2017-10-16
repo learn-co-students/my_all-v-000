bf3923471c6e22ec8abb43f36108f0069e7581de
Kevin Huang <yellow.khaos@gmail.com>
2017-10-16 01:16:21 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
 def my_all?(collection)
+  if block_given?
+    i = 0
+    return_vals = []
+    while i <collection.length
+      return_vals << yield(collection[i])
+      i = i+1
+    end
+  else
+    puts "Blocks not given"
+  end
 
+  if return_vals.include?(false)
+    return false
+  else
+    return true
+  end
 end
