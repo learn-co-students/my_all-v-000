9d95728db0be47b1c6bc08f4caa69a1c3bf84fca
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-27 18:39:55 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,21 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  block_return_values = Array.new
+  while i < collection.length
+    if yield(collection[i]) == false #this if block ends the call to all after the first time a false value is returned,
+      return false                   #instead of checking every value in the array unnecessarily
+    else
+      i += 1
+    end
+  end
+  return true
 
+#  Original "if" block:   
+#  if block_return_values.include?(false)
+#    return false
+#  else
+#    return true
+#  end
 end
