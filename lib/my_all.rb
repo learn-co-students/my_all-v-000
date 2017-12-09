901d86812a36723e3f9c6cb48bd4e10525b6fa7c
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2017-12-09 15:36:53 -0800

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0 
+  new_collection = []
+    while i < collection.length
+      new_collection << yield(collection[i])
+      i += 1
+    end 
+    if new_collection.include?(false)
+      false
+    else
+      true
+    end
 end
