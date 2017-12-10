80e255bc28cdaac9bf535ab3f0d56ea3295bbc54
natgit8 <natasha.gitlin8@gmail.com>
2017-12-09 23:03:03 -0500

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,30 @@
 require 'pry'
 
+# def my_all?(collection)
+#   i=0
+#   all = []
+#   while i < collection.length
+#     all << yield(collection[i])
+#     i+=1
+#   end
+#
+#   if all.include? false
+#     false
+#   else
+#     true
+#   end
+# end
 def my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
