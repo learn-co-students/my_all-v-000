3b1b5b0e8af964dcf46ddbc2824b32f3d8702f70
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2017-12-14 18:19:09 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
-def my_all?(collection)
-
+def my_all?(col)
+  i = 0
+  cap = []
+  while i < col.length
+    cap << yield(col[i])
+    i += 1
+  end
+  cap.include?(false)? false:true
 end
+
+# Yield Block
+my_all?([0]) { |i| i < 2 }
