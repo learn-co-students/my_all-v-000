f12df9d3cf1121d85cbd220f8be3704307779b23
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-19 20:37:43 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,11 +1,25 @@
 require 'pry'
 
-def my_all?(collection)
+def my_all?(collection)#iteration part
   i = 0
   block_return_values = []
   while i < collection.length
     block_return_values << yield(collection[i])
     i += 1
   end
+  #the block_return_values array at this point would look like
+  #[true, false, false] depending on if the elements in the collection,
+  #when compared to the comparative argument in the block.
+
+
+  #now we code sturcture for a return value of true or false
+  #because the all? method returns true or false.
+
+  if block_return_values.include?
+    (false)
+      false
+    else
+      true
+    end
 
 end
