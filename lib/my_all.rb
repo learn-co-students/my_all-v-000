ea5b7226712c967219d845037547af2bc3956085
Barbara Choy <barbchoy@gmail.com>
2018-01-21 06:18:09 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,10 @@
-require 'pry'
-
 def my_all?(collection)
+  values = []
+  i = 0
+  while i < collection.length
+    values << yield(collection[i])
+    i += 1
+  end
 
+  values.all?{|val| val == true}
 end
