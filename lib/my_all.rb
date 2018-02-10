6eba07c7258d9168be4c7b2612e4a7d24c176421
Eva Smidt <shevangeline@gmail.com>
2018-02-10 23:44:53 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -14,15 +14,3 @@ def my_all?(collection)
     true
   end
 end
-
-
-
-#def my_collect(array)
-  #i = 0
-#  collect = []
-#  while i < array.length
-#    collect << yield(array[i])
-#    i += 1
-#  end
-#  collect
-#end
