1b61f9467088b559d71e29d1d4720552957141e1
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-15 14:16:08 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
+  new_array = []
+  i = 0
 
+  while i < collection.length
+    new_array << yield(collection[i])
+    i += 1
+    end
+
+  if new_array.include?(false)
+    false
+  else
+    true
+  end
 end
