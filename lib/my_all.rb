32f1ceca08a361265607822aae7088bafff2e1c5
Beth Schofield <bethmschofield@gmail.com>
2018-02-20 23:29:25 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  results = Array.new
+  while i < collection.length
+    results << yield(collection[i])
+    i += 1
+  end
 
+  if results.include?(false)
+    false
+  else
+    true
+  end
 end
