53b63393b0fa046dd6a1d0112e4021657a14109d
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-27 21:12:32 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  new_arr = []
+  while i < collection.size
+    new_arr.push(yield collection[i])
+    i += 1
+  end
+  if new_arr.include?(false)
+    false
+  else
+    true
+  end
 end
