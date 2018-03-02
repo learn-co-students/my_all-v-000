fc2be192fb2a76bd711268c7ce7c0e30ad6ba20f
Maverick Tesini <mavericktesini@gmail.com>
2018-03-02 21:39:07 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,31 @@
 require 'pry'
 
-def my_all?(collection)
+# def my_all?(collection)
+#   i = 0
+#   block_return_values = []
+#   while i < collection.length
+#     block_return_values << yield(collection[i])
+#     i = i + 1
+#   end
+#
+#   if block_return_values.include?(false)
+#     false
+#   else
+#     true
+#   end
+# end
 
+def my_all?(array)
+    i = 0
+    storage_array = []
+    while i < array.length
+      storage_array << yield(array[i])
+      i += 1
+    end
+
+    if storage_array.include?(false)
+      false
+    else
+      true
+    end
 end
