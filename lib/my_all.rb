36e551c3c2a817789a9d2c747a4641ef8595c398
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-09 18:09:10 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0 
+  empty = []
+  while i < collection.length 
+ empty << yield(collection[i])
+  i += 1 
+end
+if empty.include?(false)
+  false
+else
+  true 
+end
 end
