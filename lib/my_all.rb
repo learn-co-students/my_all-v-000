8c53e4fe19f99dc344865e3846ee58cc5124be0b
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-04 08:14:45 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,9 @@
-require 'pry'
-
 def my_all?(collection)
-
+  block_condition_met = []
+  i = 0
+  while i < collection.length
+    block_condition_met << yield(collection[i])
+    i += 1
+  end
+  block_condition_met.all?
 end
