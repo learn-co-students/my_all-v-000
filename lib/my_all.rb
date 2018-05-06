37b1109e7a471c364806bef33469ee263afcdf13
Grace Shih <vocalise@gmail.com>
2018-05-06 18:12:10 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
-
+   i = 0
+   returnVal = []
+   while i < collection.length
+     returnVal << yield(collection[i])
+     i += 1
+   end
+   
+   if returnVal.include?(false)
+     false
+   else
+      true
+   end
 end
