e8f12d8ddfbaac0a79061a83a041864a92aca080
Richard Fenix <richard.fenix@gmail.com>
2018-05-30 03:39:38 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0 
+  block_return_values = []
+  while i < collection.length 
+    block_return_values << yield(collection[i])
+    #binding.pry
+    i += 1
+  end
 
+  if block_return_values.include?(false)
+    false
+  else
+    true 
+  end
 end
+
+#puts my_all?([4,5,3]){|i| i>2 }
