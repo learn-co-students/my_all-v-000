7709270e54115d4a83ca338276e56d972760b1db
Jed Peek <jedpeek@yahoo.com>
2018-06-04 22:56:06 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
-def my_all?(collection)
-
+def my_all?(arr)
+i = 0
+block_return = []
+  while i < arr.size
+    block_return << yield(arr[i])
+    i += 1
+  end
+  block_return.include?(false) ? false : true
 end
+
+my_all?([1,2,3]){|x| x < 4}
