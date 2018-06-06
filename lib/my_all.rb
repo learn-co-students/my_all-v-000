1d9fe22dbdcd370b374213d0a071647c683b0b41
Lorena <lorena.giddens@outlook.com>
2018-06-06 13:47:35 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
+# 1: iterate through each element using the simple lower level iterator while
 def my_all?(collection)
-
+  i = 0
+  # 3.1: declare an array before entering the while loop
+  block_return_values = []
+  while i < collection.length
+    # 2: yield each eleent in the collection to the block
+    # 3.2: shovel the return value of the block into the array
+    block_return_values << yield(collection[i])
+    i += 1
+  end
+  # 4: determine the return value of the method
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
