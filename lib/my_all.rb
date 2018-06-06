53f19a4bcf31b1497aeff27e6502aeb7b1602225
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-06 01:42:40 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
+  binding.pry
+  i = 0
+  return_block_values = []
+  while i < collection.length
+    return_block_values << yield(collection[i])
+    i += 1
+  end
 
+  if return_block_values.include?(false)
+    false
+  else
+    true
+  end
 end
