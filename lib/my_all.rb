59bf1d01b570a93cee756397da13e4e32befdf36
Sushi Chanrai <sushi@pathworks.co>
2018-07-03 11:03:41 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,11 +1,10 @@
-require 'pry'
+
 
 def my_all?(collection)
   block_return_values = []
   i = 0
   while i < collection.length
     block_return_values << yield(collection[i])
-      binding.pry
     i =i +1
   end
   block_return_values.include?(false) ?
