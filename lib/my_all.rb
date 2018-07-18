894c140926e11a6da2e8348dd180b486dc98968c
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-18 08:44:42 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,8 @@
-require 'pry'
-
 def my_all?(collection)
-
-end
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+ 
