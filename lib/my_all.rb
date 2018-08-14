2fac49d080e42c102be53ad14ff01eed1a5adad5
Steven Crouse <sdcrouse91@gmail.com>
2018-08-14 05:23:18 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,36 @@
 require 'pry'
 
-def my_all?(collection)
+#def my_all?(collection)
+  #i = 0 
+  #block_return_values = []
+  #while i < collection.length 
+    #block_return_values << yield(collection[i])
+    #i = i + 1 
+  #end
+  #if block_return_values.include?(false)
+    #false
+  #else
+    #true 
+  #end
+  
+  #This is more elegant:
+  #!block_return_values.include?(false)
+  #Or this: block_return_values.include?(false) ? false : true
+  #binding.pry
+#end
 
+#binding.pry
+
+#Arguably, this is a more elegant solution.
+def my_all?(collection)
+  i = 0 
+  
+  while i < collection.size 
+    if !yield(collection[i]) #If the block returns false even ONCE, #my_all is false.
+      return false
+    end
+    i += 1
+  end
+  
+  true #my_all is true since the block never evaluated to false
 end
