90840827f588cc68e6bc49aae8b68be71bfa8264
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-29 21:18:25 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
 def my_all?(collection)
+  block_return_values = Array.new
+  i = 0
+  while i < collection.size
+    block_return_values.push(yield(collection[i]))
+    i += 1
+  end
 
+  if block_return_values.include?(false)
+    return false
+  else
+    return true
+  end 
 end
+
+
+my_all?([1,2,3]) {|i| i < 2}
