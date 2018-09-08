80994706350d4808a57173771ccd7a131f9200dc
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 16:35:58 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
 def my_all?(collection)
-
+  if collection != []
+    block_return_values = []
+    i = 0
+    while i < collection.length
+      block_return_values << yield(collection[i])
+      i = i + 1 
+    end
+  
+    if block_return_values.include?(false)
+      false
+    else
+      true 
+    end
+  else
+    "No data in collection"
+  end
 end
