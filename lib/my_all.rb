c3d36164b7581b3e884d98e8f9d20c67b35e89ea
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-15 00:49:37 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,5 +7,12 @@ return_array = []
     return_array << yield(collection[i])
     i += 1
   end
-  !return_array.include?(false)
+  y = 0
+  while y < return_array.length
+    return false if return_array[y] == false
+    y += 1
+  end
+  true
+ # !return_array.include?(false) this works but I wanted 
+ # to implement a version without the use of any high level enumerators
 end
