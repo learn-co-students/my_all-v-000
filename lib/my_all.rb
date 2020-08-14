531884bdd8009f2e1fe3346d14c799546be4f8fc
Charles Okwechime <okwechimeamechi@gmail.com>
2020-08-14 03:29:31 -0400

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,9 +7,7 @@ def my_all?(collection)
    block_return_values  << yield(collection[i])
     i = i + 1
   end
-  if block_return_values.include?(false)
-    false
-  else
-    true
-  end
+  block_return_values.include?(false) ? false : true
 end
+
+
